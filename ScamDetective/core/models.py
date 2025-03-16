8c67dfed488559f66e770f3961ea9fa8c6cda736
Scott Jeppson <sfjepp46@gmail.com>
2025-03-16 12:08:54 -0600

    Adding models for keywords and country codes

--- ScamDetective/core/models.py
+++ ScamDetective/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Keyword(models.Model):
+    id = models.AutoField(primary_key=True)
+    body = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.body
+     
+    def compare(self, text):
+        return self.body in text
+
+class CountryCodes(models.Model):
+    id = models.AutoField(primary_key=True)
+    country = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.country
+
