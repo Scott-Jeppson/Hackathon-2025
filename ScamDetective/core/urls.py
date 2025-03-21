ef5cdcdda93908761e43caac569b017b14490033
Scott-Jeppson <sfjepp46@gmail.com>
2025-03-20 22:40:17 -0600

    Adding images for django and creating urls for input and output.

--- ScamDetective/core/urls.py
+++ ScamDetective/core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # put your paths here
+    path('', views.home, name='index'),
+    path('/input', views.input, name='input'),
+    path('/output', views.output, name='output'),
 ]
--- ScamDetective/core/views.py
+++ ScamDetective/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html')
+
+def input(request):
+    return render(request, 'core/Input.html')
+
+def output(request):
+    return render(request, 'core/Output.html')
