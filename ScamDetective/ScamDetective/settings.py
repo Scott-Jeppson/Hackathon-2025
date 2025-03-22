a24a5ad9388d8dbfbe1e44f63914eed5ce6e78c0
Scott-Jeppson <sfjepp46@gmail.com>
2025-03-21 18:58:37 -0600

    Got output working for the most part. Still need to add phrases and keywords for scams

--- ScamDetective/ScamDetective/settings.py
+++ ScamDetective/ScamDetective/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
 ROOT_URLCONF = 'ScamDetective.urls'
--- ScamDetective/core/views.py
+++ ScamDetective/core/views.py
@@ -3,9 +3,6 @@ from .models import Keyword, CountryCode, Phrase
 from django.http import HttpResponse
 from django.contrib import messages
 
-CURRENT_SCORE = 0
-CURRENT_CODE = None
-
 # Create your views here.
 def home(request):
     return render(request, 'core/index.html')
@@ -14,7 +11,9 @@ def input(request):
     if request.method == 'GET':
         return render(request, 'core/Input.html')
     elif request.method == 'POST':
-        CURRENT_SCORE = 0
+        currentScore = 0
+        currentCode = None
+
         # Get the data from the form
         email = request.POST['email']
         number = request.POST['phoneNumber']
@@ -29,11 +28,10 @@ def input(request):
         if '@' in email:
             for phrase in phrases:
                 if phrase.compare(message):
-                    CURRENT_SCORE += phrase.getScore()
+                    currentScore += phrase.getScore()
             for keyword in keywords:
                 if keyword.compare(message):
-                    CURRENT_SCORE += keyword.getScore()
-            return redirect('/output')
+                    currentScore += keyword.getScore()
         else:
             if not number:
                 messages.error(request, 'Please enter a phone number or an email address')
@@ -45,21 +43,49 @@ def input(request):
             number = number.lstrip('+')
             parts = number.split(' ', 1)
             if len(parts) == 2:
-                CURRENT_CODE = parts[0]
+                currentCode = parts[0]
                 number = parts[1]
             else:
                 messages.error(request, 'Invalid phone number format. Please include a space after the country code.')
-                return render(request, 'core/index.html')
+                return render(request, 'core/Input.html')
             for phrase in phrases:
                 if phrase.compare(message):
-                    CURRENT_SCORE += phrase.getScore()
+                    currentScore += phrase.getScore()
             for keyword in keywords:
                 if keyword.compare(message):
-                    CURRENT_SCORE += keyword.getScore()
-            return redirect('/output')
+                    currentScore += keyword.getScore()
+
+        # Store current score and current code in cookies
+        response = redirect('/output')
+        response.set_cookie('currentScore', currentScore, samesite='Lax')
+        response.set_cookie('currentCode', currentCode, samesite='Lax')
+        response.set_cookie('currentNumber', number, samesite='Lax')
+        response.set_cookie('currentEmail', email, samesite='Lax')
+        return response
 
 def output(request):
-    return render(request, 'core/Output.html')
+    # Retrieve current score and current code from cookies
+    currentScore = int(request.COOKIES.get('currentScore', 0))
+    currentCode = request.COOKIES.get('currentCode', None)
+    currentNumber = request.COOKIES.get('currentNumber', None)
+    currentEmail = request.COOKIES.get('currentEmail', None)
+
+    phone = False
+    source = currentEmail
+    if currentNumber:
+        phone = True
+        c = CountryCode.objects.filter(code=currentCode).first()
+        if c:
+            source = c.__str__()
+        else:
+            source = "Unknown Country"
+    
+    if request.method == 'GET':
+        return render(request, 'core/Output.html', {
+            'percentage': currentScore,
+            'phone': phone,
+            'source': source,
+        })
 
 def secretPage(request):
     if request.method == 'GET':
